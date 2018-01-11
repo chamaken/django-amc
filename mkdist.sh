30db8f07cebda31739fb742da0b7cf64e5c75369
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2018-01-11 09:24:06 +0900

    env: fix README suffix

--- mkdist.sh
+++ mkdist.sh
@@ -5,7 +5,7 @@ CWD=$(pwd)
 TMPDIR=$(mktemp -d)
 trap "rm -rf $TMPDIR" 1 2 15
 
-cp -au amc LICENSE MANIFEST.in README.md requirements.txt setup.py $TMPDIR
+cp -au amc LICENSE MANIFEST.in README.rst requirements.txt setup.py $TMPDIR
 
 cd $TMPDIR
 $PYTHON setup.py sdist
